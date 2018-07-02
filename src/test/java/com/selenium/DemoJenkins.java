eb2051919f7de1f6c4cf1deee4e812e418767d75
jalda123 <sateesh.jalda2020@gmail.com>
2018-07-02 16:36:41 +0530

    firstcode

---
+++ src/test/java/com/selenium/DemoJenkins.java
@@ -0,0 +1,13 @@
+package com.selenium;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkins {
+	
+	@Test
+	public void demo() {
+		
+		System.out.println("This is jenkins code");
+	}
+
+}
